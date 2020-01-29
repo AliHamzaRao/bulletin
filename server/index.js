ca556aa8de797e4b75dabad48907aa52bab481f2
AliHamzaRao <hamzaraoa010@gmail.com>
2020-01-29 12:52:43 +0500

    Bullet-in Security

--- server/index.js
+++ server/index.js
@@ -100,7 +100,7 @@ app.get("/api", (req, res) => {
 });
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-  console.log("server is listening on port 3000/5000/6000");
+  console.log("server is listening on port", PORT);
 });
 
 app;
